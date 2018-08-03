56156f537a7382db0630adf8263f334eecc2f85a
Frederic Gessler <frederic.gessler@epfl.ch>
2018-08-03 19:04:55 +0200

    Environnement set up

---
+++ test/interpret.test.js
@@ -0,0 +1,21 @@
+/*
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
+
+*/
+var assert = require('assert');
+var expect = require('chai').expect;
+describe('Blobs', function() {
+  
+  it('should list a SINGLE blob on /blob/<id> GET');
+  it('should add a SINGLE blob on /blobs POST');
+  it('should update a SINGLE blob on /blob/<id> PUT');
+  it('should delete a SINGLE blob on /blob/<id> DELETE');
+});
+it('should list ALL blobs on /blobs GET', () => expect(43).to.equal(42));
