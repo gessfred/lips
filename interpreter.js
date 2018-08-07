2bed502066b6b7393f0e0013de15f34148f25c9b
Frederic Gessler <frederic.gessler@epfl.ch>
2018-08-07 20:59:18 +0200

    cleaned traces

--- interpreter.js
+++ interpreter.js
@@ -11,42 +11,32 @@ const wrapper = function(value, env) {
 const flat = (x) => [].concat(x)
 
 const eval = function(x, env) {
-    console.log('closure:'+x+'   ::   '+env.dump())
     const [head, ...tail] = x
     if(tail.length == 0) {
-        console.log('data')
         if(int.test(x)) return wrapper(parseInt(x), env)
         else return wrapper(env.lookup(head), env)
     }
     else {
         switch(head) {
             case 'if': {
-                console.log('if')
                 const [cond, ...rest] = tail
                 const [then, ...other] = rest
-                console.log('if:' + cond + ' then:'+then + ' else:' + other)
-                console.log(env.dump())
-                console.log('cond = ' + eval(flat(cond), env).value)
                 if(eval(flat(cond), env).value != 0) return eval(flat(then), env)
                 else return eval(flat(other), env)
             } 
             case 'lambda': {
-                console.log('lambda')
                 const [params, body] = tail
                 return wrapper((function(args) {
-                    console.log('wrapping... ' + args + ' on ' + params + ' for ' + body)
                     return eval(body, env.extendMulti(flat(params), flat(args))).value
                 }), env)
             }
             case 'def': {
-                console.log('def')
                 const [[name, ...args], body, ...expr] = tail
                 if(args.length > 0) 
                     if(expr.length == 0)return eval(['def', [name], ['lambda', args, body]], env)
                     else return eval(['def', [name], ['lambda', args, body]], env)
                 else {
                     const newEnv = env.extendRec(name, function(env1) {
-                        console.log(name + 'might be problem ' + body)
                         return eval(flat(body), env1).value
                     })
                     return /*(expr.length > 0) ? eval(expr, newEnv) :*/ wrapper('def ' + name, newEnv)
@@ -56,7 +46,6 @@ const eval = function(x, env) {
 
             }
             default: {
-                console.log('op')
                 return wrapper(eval(flat(head), env).value(tail.map((operand) => eval(flat(operand), env).value)), env)
             }
         }
