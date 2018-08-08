6549227ec55118cbb46b38839564b2715449521d
Frederic Gessler <frederic.gessler@epfl.ch>
2018-08-08 13:53:38 +0200

    environment flattened for efficient lookup, might need to just parachute original lookup function

--- environment.js
+++ environment.js
@@ -1,16 +1,22 @@
-const environment = {
+const Environment = function(space){
+    const bindings = space
+    return {
     lookup: function(name) {
-        throw new Error(name + ' not found')
+        const g = bindings.get(name)
+        if(typeof g == 'undefined') throw new Error(name + ' not found')
+        else return g
     },
     dump: function() {
-        return []
+        return bindings.keys()//[]
     },
     extend: function(name, value) {
         const enclosing = this
         const exposing = Object.create(this)
-        exposing.lookup = (symbol) => symbol === name ? value : enclosing.lookup(symbol)
-        exposing.dump = () => [name].concat(enclosing.dump())
-        return exposing
+        const clone = new Map(bindings)
+        clone.set(name, value)
+        //exposing.lookup = (symbol) => symbol === name ? value : enclosing.lookup(symbol)
+        //exposing.dump = () => [name].concat(enclosing.dump())
+        return Environment(clone)
     },
     extendMulti: function(params, values) {
         var [p, ...ps] = params
@@ -22,8 +28,26 @@ const environment = {
         const that = Object.create(this)
         that.lookup = (symbol) => symbol === name ? expr(that) : enclosing.lookup(symbol)
         that.dump = () => [name].concat(enclosing.dump())
+        /*const that = Environment(new Map(bindings))
+
+        that.lookup = function(symbol) {
+            const cpy = new Map(bindings)
+            cpy.set(name, expr(that))
+            console.log(name + ': ' + expr(that))
+            console.log(Environment(cpy).dump())
+            return Environment(cpy).lookup(symbol)
+            
+            if(symbol === name) {
+                return expr(that)
+            }
+            else {
+                return this.lookup(symbol)
+            }
+        }*/
         return that
     }
-}
+}}
+
+const environment = Environment(new Map())
 
 module.exports = environment;
