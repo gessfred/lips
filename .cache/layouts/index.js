42fed523667979cc7c252ff5d33ab62607bc64c2
Frederic Gessler <frederic.gessler@epfl.ch>
2018-08-10 02:44:02 +0200

    error cached files corrected

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/fredericgessler/Documents/frontend/lips/src/layouts/index.js"
-  import data from "/Users/fredericgessler/Documents/frontend/lips/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
