531e07c452974c73e48f979152e5f9341dabf79e
Frederic Gessler <frederic.gessler@epfl.ch>
2018-08-13 12:49:01 +0200

    editor layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,41 @@ import './index.css'
 const Icon = (props) => (
 	<button className='icon'>
 		<div className='logo'>( )</div>
+		<div className='logotext'>lips</div>
 	</button>
 )
+
+class Editor extends React.Component {
+	render() {
+		return (
+			<textarea className='editor' ref='editor'/>
+		)
+	}
+}
+
+class Console extends React.Component {
+	render() {
+		return (
+			<div className='console'>
+				okokok
+				textarea
+				vsdnld
+			</div>
+		)
+	}
+}
 
 class App extends React.Component {
 	render() {
 		return (
-			<div>
+			<div className='root'>
 		    <div className='topnav'>
 					<Icon/>
-		    </div>
-		    <div>
 
+		    </div>
+		    <div className='main'>
+					<Editor />
+					<Console />
 		    </div>
 		  </div>
 		)
