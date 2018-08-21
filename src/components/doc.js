9ae7450011af440e1defacbad8d4d95b7b2e03da
Frederic Gessler <frederic.gessler@epfl.ch>
2018-08-21 15:09:06 +0200

    changed doc layout

--- src/components/doc.js
+++ src/components/doc.js
@@ -11,17 +11,21 @@ const Documentation = (props) => (
 	<div className='documentation'>
 		<h1 className='biglogo'>( )</h1>
 		<h1 className='biglogotext'>lips</h1>
-		<h3>(learn code)</h3>
-		<h3>(hack code)</h3>
-		<h3>(love code)</h3>
-		<h1>Language reference</h1>
+		<div className='motto'>
+			<h3>(learn code)</h3>
+			<h3>(hack code)</h3>
+			<h3>(love code)</h3>
+		</div>
+		<div className='reference'>
+			<h1>Language reference</h1>
 
-		<Code kw='if' args='expr then else'/>
-		<Code kw='lambda' args='formals...'/>
-		<Code kw='def' args='name expr'/>
-		<Code kw='def' args='name args... expr'/>
+			<Code kw='if' args='expr then else'/>
+			<Code kw='lambda' args='formals...'/>
+			<Code kw='def' args='name expr'/>
+			<Code kw='def' args='name args... expr'/>
 
-		<Code kw='case' args='cond (vali expri)... (else expr)' />
+			<Code kw='case' args='cond (vali expri)... (else expr)' />
+		</div>
 	</div>
 )
 
